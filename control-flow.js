c69efa590592af982a9ed9bdbb54005da4de0c82
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2026-07-04 12:28:12 +0100

    Add switch statements and random number generation

--- control-flow.js
+++ control-flow.js
@@ -80,4 +80,113 @@ Write a JavaScript if...else if...else statement that checks the score and print
 6 = Sunday
 
 */
-const day = 0;
+
+const day = 9;
+
+switch(day) {
+    case 0: 
+       console.log("Monday");
+       break; 
+    case 1:
+        console.log("Tuesday");
+        break;
+    case 2: 
+        console.log("Wednesday");
+        break;
+    case 3:
+        console.log("Thursday");
+        break;
+    case 4: 
+        console.log("Friday");
+        break;
+    case 5: 
+        console.log("Saturday");
+        break;
+    case 6: 
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Invalid day");
+}
+
+/* 
+Write a program that checks a user’s role using a switch statement.
+The role can be "admin", "editor", "viewer", or any other value.
+Print:
+"Full access granted" for "admin"
+"Limited edit access" for "editor"
+"Read-only access" for "viewer"
+"No access" for any other role
+
+*/
+
+const userRole = "editor";
+
+switch (userRole) {
+    case "admin":
+        console.log("Full access granted");
+        break;
+    case "editor":
+        console.log("Limited edit access");
+        break;
+    case "viewer":
+        console.log("Read-only access");
+        break;
+    default:
+        console.log("No access");
+}
+
+/* 
+============ CLASS ACTIVITY ===============
+
+Create a program that takes a variable called signal.
+If signal is "red", print "Stop"
+If "yellow", print "Get ready"
+If "green", print "Go"
+For any other value, print "Invalid signal"
+
+*/
+
+// The fall-through behaviour
+// admins and editors ==> print "Can edit document"
+// viewers => print "No edit access"
+// "No access to document" for any other role
+
+const userAccess = "admin";
+
+switch (userAccess) {
+    case "admin":
+    case "editor":
+        console.log("Can edit document");
+        break;
+    case "viewer":
+        console.log("No edit access");
+        break;
+    default: 
+        console.log("No access to document");
+}
+
+const mark = 50;
+
+// 0 - 14 =====> print "Poor performance"  
+// 15 - 29 =====> print "Fair performance"  
+// 30 - 39 =====> print "Good performance"  
+// 40 - 50 =====> print "Great performance"  
+// otherwise print "Not a valid grade"
+
+switch (true) {
+    case mark >= 0 && mark <= 14:
+        console.log("Poor performance");
+        break;
+    case mark >= 15 && mark <= 29:
+        console.log("Fair performance");
+        break;
+    case mark >= 30 && mark <= 39:
+        console.log("Good performance");
+        break;
+    case mark >= 40 && mark <= 50:
+        console.log("Great performance");
+        break;
+    default:
+        console.log("Not a valid grade");
+}
--- math-method.js
+++ math-method.js
@@ -28,3 +28,36 @@ console.log(Math.pow(7, 2));
 
 const myNum = 10;
 console.log(Math.pow(myNum, 3));
+
+
+// Generating random number
+/* 
+Math.floor(Math.random() * (max - min) + min)
+This works with min (included) and max (excluded)
+
+Math.floor(Math.random() * (max - min + 1)) + min
+This works with both min and max inclusive
+
+*/
+
+console.log((2 * 7 + 6)); // 20
+console.log((2 * (7 + 6))); // 26
+
+
+
+// 5 and 89 (89 is excluded)
+{ // 0.9999888 * 88 = 87.999 + 5 = 88.999 = 88
+    const maxNumber = 89;
+    const minNumber = 5; 
+    const result = Math.floor(Math.random() * (maxNumber - minNumber) + minNumber);
+    console.log(result);
+}
+
+
+{
+    const maxNumber = 999999;
+    const minNumber = 100000; 
+    const result = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+
+    console.log("Result: ", result);
+}
